f28130641c413ab4330033d29d0442ef3fec9022
mi4en <marionikolov87@gmail.com>
2019-06-20 11:14:27 +0300

    update example;

--- BigONotations/timingOurCode.js
+++ BigONotations/timingOurCode.js
@@ -7,11 +7,14 @@ function addUpTo(n) {
 
 	return sum;
 }
+// here we have O(n) meaning that due to the n-dependand count of operations performance (in time) will exponentially grow with n
 
 // Same as the first function but with different syntax - around 0 seconds on my local machine
 function addUpTo(n) {
 	return n * (n + 1) / 2;
 }
+// here we have O(1) meaning that due to the constant amount of operations performed regardless of n
+// the performance will be pretty same or just to simplify things a constant
 
 // We can see how fast each function performs
 var t1 = performance.now();
