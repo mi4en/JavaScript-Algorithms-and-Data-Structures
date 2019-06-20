c6e565fa9a7614f03967d4bf6e4272d69ab57fdd
mi4en <marionikolov87@gmail.com>
2019-06-20 09:04:14 +0300

    add function with diff syntax; add time measurment for comparison;

--- BigONotations/timingOurCode.js
+++ BigONotations/timingOurCode.js
@@ -1,3 +1,4 @@
+// Simple function that add nums up to N - between 1-4 seconds on my local machine
 function addUpTo(n) {
 	let sum = 0;
 	for (let index = 0; index < n; index++) {
@@ -7,4 +8,14 @@ function addUpTo(n) {
 	return sum;
 }
 
-console.log(addUpTo(6));
+// Same as the first function but with different syntax - around 0 seconds on my local machine
+function addUpTo(n) {
+	return n * (n + 1) / 2;
+}
+
+// We can see how fast each function performs
+var t1 = performance.now();
+addUpTo(1000000000);
+var t2 = performance.now();
+
+console.log(`Time elapsed: ${(t2 - t1) \ 1000} seconds.`);
